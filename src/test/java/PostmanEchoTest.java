bf28f8078456325f24e14e2b01293806fffe3783
Maks S <Gmagz@yandex.ru>
2023-03-24 15:31:23 +0300

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+
+    @Test
+    void shouldReturnData() {
+
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Test") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Test"))
+                .body(matchesJsonSchemaInClasspath("postmanecho.schema.json")); //необязательная проверка, требует настройки схемы, если без этой проверки схема не нужна.
+
+    }
+}
+
+
+
